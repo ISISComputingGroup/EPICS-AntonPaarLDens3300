342ad05ca3ffbeb3a3ce8f60a11e9d077da39e8c
George Ryall <George.Ryall@stfc.ac.uk>
2024-10-24 17:23:24 +0100

    Add Lewis emulator

---
+++ system_tests/lewis_emulators/antonpaarldens3300/__init__.py
@@ -0,0 +1,5 @@
+from .device import SimulatedAntonpaarldens3300
+from ..lewis_versions import LEWIS_LATEST
+
+framework_version = LEWIS_LATEST
+__all__ = ['SimulatedAntonpaarldens3300']
---
+++ system_tests/lewis_emulators/antonpaarldens3300/interfaces/__init__.py
@@ -0,0 +1,3 @@
+from .stream_interface import Antonpaarldens3300StreamInterface
+
+__all__ = ['Antonpaarldens3300StreamInterface']
